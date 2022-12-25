7cb9155af70e4bb256cb4cf9f412f4d07528b1c6
anaele nnaemeka <anaelennaemeka@gmail.com>
2022-12-25 10:55:48 +0100

    master Branch

--- src/App.vue
+++ src/App.vue
@@ -1,98 +1,15 @@
 <template>
   <div id="appContainer">
-    <h1>ToDo List</h1>
-    <form @submit.prevent="addTodos()">
-      <input v-model="newTodo" />
-      <button class="waves-effect waves-light btn">Add ToDo</button>
-    </form>
-    <div id="todoList">
-      <div
-        :class="{ completed: todo.complete }"
-        v-for="(todo, index) in todos"
-        :key="index"
-        @click="completedTodo(todo)"
-      >
-        <div class="row">
-          <div class="col s12 m6">
-            <div class="card teal teal lighten-1">
-              <div class="card-content white-text">
-                <span class="card-title">{{ todo.text }}</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <p v-if="todos.length === 0">Empty list, please enter some tasks</p>
-    <button
-      class="btn waves-effect waves-light red lighten-2"
-      v-if="todos.length !== 0"
-      @click="removeTodos"
-    >
-      Remove All Todos
-    </button>
+    <h1>This is are Basic Vue Application</h1>
+    <ul>
+      <li>Todo Application Using Vue</li>
+    </ul>
   </div>
 </template>
 <script>
 import { ref } from "vue";
 export default {
   name: "App",
-  setup() {
-    const newTodo = ref("");
-    const initialLoadData = [
-      {
-        complete: false,
-        text: "Create todo list functionality",
-      },
-    ];
-    let stroedTodos;
-    localStorage.getItem("todos")
-      ? (stroedTodos = JSON.parse(localStorage.getItem("todos")))
-      : (stroedTodos = initialLoadData);
-    const todos = ref(stroedTodos);
-    function addTodos() {
-      if (newTodo.value !== "") {
-        todos.value.push({
-          complete: false,
-          text: newTodo.value,
-        });
-        newTodo.value = "";
-        updateStorage();
-      }
-    }
-    function removeTodos() {
-      todos.value.splice(0, todos.value.length);
-      updateStorage();
-    }
-    function completedTodo(todo) {
-      todo.complete = !todo.complete;
-      updateStorage();
-    }
-    function updateStorage() {
-      localStorage.setItem("todos", JSON.stringify(todos.value));
-    }
-    return {
-      completedTodo,
-      removeTodos,
-      addTodos,
-      todos,
-      newTodo,
-    };
-  },
 };
 </script>
-<style>
-#todoList {
-  margin: 2%;
-  padding: 0%;
-}
-.card-content:hover {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 2);
-}
-#todoList div.completed {
-  opacity: 0.5;
-}
-#appContainer {
-  padding: 2%;
-}
-</style>
+<style></style>
